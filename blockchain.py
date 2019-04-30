1e5f8ff2c50f368c85f1c382e69a80143e163cf8
rmar72 <rsolm72@gmail.com>
2019-04-30 16:25:28 -0600

    flask app standup with new transaction endpoint fleshed out

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
 from time import time
+from textwrap import dedent
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
@@ -103,3 +107,41 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_indentifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    #Check that the required fields are in the POST'ed data
+    required; = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    # Create new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
